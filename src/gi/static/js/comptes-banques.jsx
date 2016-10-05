670f0c6311cb3afc178bb46b0b8def479d7060f7
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2016-10-05 16:53:32 +0200

    [WIP] Tab "Banques de dépôt" seems OK

--- src/gi/static/js/comptes-banques.jsx
+++ src/gi/static/js/comptes-banques.jsx
@@ -9,6 +9,7 @@ var ComptesBanquesDepot = React.createClass({
     getInitialState() {
         return {
             active: this.props.initActive,
+            depositBankList: Array(),
         }
     },
 
@@ -19,11 +20,11 @@ var ComptesBanquesDepot = React.createClass({
     },
 
     componentDidMount() {
-        // Get historyTableData
-        // var computeHistoryTableData = (historyTableData) => {
-        //     this.setState({historyTableData: historyTableData})
-        // }
-        // fetchAuth({getAPIBaseURL + "bdc/"}, 'get', computeHistoryTableData)
+        // Get depositBankList
+        var computeBankDepositList = (depositBankList) => {
+            this.setState({depositBankList: _.sortBy(depositBankList, (item) => { return item.label })})
+        }
+        fetchAuth(getAPIBaseURL + "deposit-banks/", 'get', computeBankDepositList)
     },
 
     render() {
@@ -32,9 +33,35 @@ var ComptesBanquesDepot = React.createClass({
             'hidden': !this.state.active,
         })
 
+        var divDepositBanks = _.map(this.state.depositBankList,
+            (item) => {
+                return (
+                    <div key={item.value} className="col-md-offset-1 col-md-10">
+                        <div className="panel panel-info">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">{item.label}</h3>
+                            </div>
+                            <div className="panel-body">
+                                <div className="row">
+                                    <div className="col-md-offset-1 col-md-2">
+                                        <a href={"/banques/history/" + item.shortLabel} className="btn btn-default">{__("Historique")}</a>
+                                    </div>
+                                    <div className="col-md-offset-1 col-md-2 col-sm-4">
+                                        <a href={"/banques/rapprochement/" + item.shortLabel} className="btn btn-info">{__("Rapprocher")}</a>
+                                    </div>
+                                    <div className="col-md-offset-1 col-md-2 col-sm-4">
+                                        <a href={"/banques/virement/" + item.shortLabel} className="btn btn-default">{__("Faire un virement")}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+
         return (
             <div className={activeOrNot}>
-                {'ComptesBanquesDepot'}
+                {divDepositBanks}
             </div>
         )
     }
