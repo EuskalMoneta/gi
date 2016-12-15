35ab5394ef092173d9cdf6275ac727f4fe0e7075
Martineau <a.martineau@net.estia.fr>
2016-12-15 16:05:36 +0100

    [IMP] Set all button in line

--- src/gi/static/js/operations.jsx
+++ src/gi/static/js/operations.jsx
@@ -31,17 +31,21 @@ var Operations = React.createClass({
                 </div>
                 <div className="panel-body">
                     <div className="row">
-                        <div className="col-md-offset-1 col-md-5">
-                            <a href="/operations/entrees-euro" className="btn btn-default">{__("Entrées dans la Caisse € d'E.M.")}</a>
-                        </div>
                         <div className="col-md-5">
-                            <a href="/operations/entrees-eusko" className="btn btn-info">{__("Entrées dans la Caisse Eusko d'E.M.")}</a>
+                            <a href="/operations/entrees-euro" className="btn btn-default">{__("Entrées dans la Caisse € d'E.M.")}</a>
                         </div>
                     </div>
                     <div className="row margin-top">
-                        <div className="col-md-offset-1 col-md-5">
+                        <div className="col-md-5">
+                            <a href="/operations/entrees-eusko" className="btn btn-default">{__("Entrées dans la Caisse Eusko d'E.M.")}</a>
+                        </div>
+                    </div>
+                    <div className="row margin-top">
+                        <div className="col-md-5">
                             <a href="/operations/reconversions" className="btn btn-default">{__("Reconversions")}</a>
                         </div>
+                    </div>
+                    <div className="row margin-top">
                         <div className="col-md-4">
                             <a href="/operations/depots-retraits" className="btn btn-default">{__("Dépôts et retraits")}</a>
                         </div>
