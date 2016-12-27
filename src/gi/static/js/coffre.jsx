53c45cc6add92b49bc02f90bd92b98c8d921c008
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2016-12-27 14:52:32 +0100

    [IMP] #716 - Le coffre et le compte de transit ont leurs soldes affichés dans la page directement, sans entrer dans l'historique.

--- src/gi/static/js/coffre.jsx
+++ src/gi/static/js/coffre.jsx
@@ -10,21 +10,45 @@ class CoffrePage extends React.Component {
 
         // Default state
         this.state = {
+            stockBilletsData: undefined,
+            compteTransitEuroData: undefined,
         }
 
+        // Get System Accounts Summaries
+        var computeManagerData = (data) => {
+            this.setState({
+                stockBilletsData: _.filter(data, (item) => { return item.type.id == "stock_de_billets" })[0],
+                compteTransitEuroData: _.filter(data, (item) => { return item.type.id == "compte_de_transit" })[0],
+            })
+        }
+        fetchAuth(getAPIBaseURL + "system-accounts-summaries/", 'get', computeManagerData)
     }
 
     render() {
         return (
             <div className="col-md-10">
-                <Coffre />
-                <CompteDeTransit />
+                <Coffre data={this.state.stockBilletsData} />
+                <CompteDeTransit data={this.state.compteTransitEuroData} />
             </div>
         )
     }
 }
 
 var Coffre = React.createClass({
+    getInitialState() {
+        return {
+            balance: '',
+            currency: '',
+        }
+    },
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.data) {
+            this.setState({balance: nextProps.data.balance,
+                           currency: nextProps.data.currency})
+        }
+    },
+
     render() {
         return (
             <div className="panel panel-info">
@@ -33,13 +57,19 @@ var Coffre = React.createClass({
                 </div>
                 <div className="panel-body">
                     <div className="row">
-                        <div className="col-md-offset-1 col-md-2">
+                        <div className="col-md-8 col-sm-4">
+                            <label className="control-label col-md-3">{__("Solde")} :</label>&nbsp;
+                            <span className="col-md-5">{this.state.balance + " " + this.state.currency}</span>
+                        </div>
+                        <div className="col-md-4">
                             <a href="/coffre/history" className="btn btn-default">{__("Historique")}</a>
                         </div>
-                        <div className="col-md-offset-1 col-md-2 col-sm-4">
+                    </div>
+                    <div className="row margin-top">
+                        <div className="col-md-offset-2 col-md-2 col-sm-4">
                             <a href="/coffre/entree" className="btn btn-info">{__("Entrée")}</a>
                         </div>
-                        <div className="col-md-offset-1 col-md-2 col-sm-4">
+                        <div className="col-md-offset-2 col-md-2 col-sm-4">
                             <a href="/coffre/sortie" className="btn btn-default">{__("Sortie")}</a>
                         </div>
                     </div>
@@ -50,6 +80,20 @@ var Coffre = React.createClass({
 })
 
 var CompteDeTransit = React.createClass({
+    getInitialState() {
+        return {
+            balance: '',
+            currency: '',
+        }
+    },
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.data) {
+            this.setState({balance: nextProps.data.balance,
+                           currency: nextProps.data.currency})
+        }
+    },
+
     render() {
         return (
             <div className="panel panel-info">
@@ -58,7 +102,11 @@ var CompteDeTransit = React.createClass({
                 </div>
                 <div className="panel-body">
                     <div className="row">
-                        <div className="col-md-offset-1 col-md-4 col-sm-4">
+                        <div className="col-md-8 col-sm-4">
+                            <label className="control-label col-md-3">{__("Solde")} :</label>&nbsp;
+                            <span className="col-md-5">{this.state.balance + " " + this.state.currency}</span>
+                        </div>
+                        <div className="col-md-4">
                             <a href="/comptedetransit/history" className="btn btn-default">{__("Historique")}</a>
                         </div>
                     </div>
